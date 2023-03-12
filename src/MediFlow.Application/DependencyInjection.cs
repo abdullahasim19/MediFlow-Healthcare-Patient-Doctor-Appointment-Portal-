df018c957402d6ee5317f3ff644b069a85e4d353
Abdullah Asim <abdullahasimlhe@gmail.com>
2023-03-12 10:15:00 +0500

    chore: configure application dependency injection

---
+++ src/MediFlow.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using MediFlow.Application.Common.Behaviors;
+using System.Reflection;
+
+namespace MediFlow.Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+        
+        services.AddMediatR(cfg => {
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(AuthorizationBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(LoggingBehavior<,>));
+            cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+        });
+        
+        return services;
+    }
+}
